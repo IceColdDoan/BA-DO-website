26746847de8c44973a90fdabe709ac94109a7b98
doanbenson <doanbenson@gmail.com>
2024-03-28 19:40:28 +0000

    adding about page gotta plan out what I wnt the webpage to look like now

---
+++ src/components/about/About.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './About.module.css'
+
+function About() {
+  return (
+    <>
+        <div className={styles.container}>
+            <h3 className={styles.title}>
+                Features in App
+            </h3>
+        </div>
+    </>
+  )
+}
+
+export default About
